60a2e3fd9fa3a31909fff3699de1c69261b2eb7d
Jeff Wyckoff <jokerswild663@gmail.com>
2015-11-02 23:37:59 -0800

    adding condition to clone vundle plugin before .vimrc copy

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,12 @@
 
 projectDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+if [ ! -d "$HOME/.vim/bundle/vundle" ]; then
+	git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+fi
+
 if [ ! -f "$HOME/.vimrc" ]; then
 	cp -v "$projectDir/vimConfig/.vimrc" "$HOME/.vimrc"
 else
 	echo "vim config already in place"
-fi
-
-if [ ! -d "$HOME/.vim/bundle/vundle" ]; then
-	git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
